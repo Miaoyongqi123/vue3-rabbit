d384e1acc30df58168b5dfd31356a701f20054bd
yongqi <yongqi9995@gmail.com>
2023-10-17 00:46:36 +0800

    实现吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,10 +3,12 @@
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
+  <LayoutFixed />
 </template>
 
 <script setup>
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
