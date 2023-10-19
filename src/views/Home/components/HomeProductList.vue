e223a45f8b0671de6fc3efbb52efd6f04c78d424
yongqi <yongqi9995@gmail.com>
2023-10-19 22:48:15 +0800

    home-GoodsItem组件封装

--- src/views/Home/components/HomeProductList.vue
+++ src/views/Home/components/HomeProductList.vue
@@ -1,14 +1,15 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
-import{getGoodsAPI} from '@/api/home.js'
-import { ref ,onMounted} from 'vue'
+import GoodsItem from './GoodsItem.vue'
+import { getGoodsAPI } from '@/api/home.js'
+import { ref, onMounted } from 'vue'
 const goodsProduct = ref([])
 const getGoodsList = async () => {
   const res = await getGoodsAPI()
   console.log(res);
   goodsProduct.value = res.result
 }
-onMounted(()=>{
+onMounted(() => {
   getGoodsList()
 })
 //获取数据列表
@@ -27,15 +28,11 @@ onMounted(()=>{
           </strong>
         </RouterLink>
         <ul class="goods-list">
-          <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img :src="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+          <li v-for="goods in cate.goods" :key="goods.id">
+            <GoodsItem :goods="goods" />
           </li>
         </ul>
+
       </div>
     </HomePanel>
   </div>
@@ -45,6 +42,7 @@ onMounted(()=>{
 .home-product {
   background: #fff;
   margin-top: 20px;
+
   .sub {
     margin-bottom: 2px;
 
@@ -128,41 +126,7 @@ onMounted(()=>{
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
+   
   }
 }
 </style>
