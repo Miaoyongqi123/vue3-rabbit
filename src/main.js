854198ed052fb526f02dceca7e312f1f69bad559
yongqi <yongqi9995@gmail.com>
2023-10-19 22:12:57 +0800

    "图片懒加载优化"

--- src/main.js
+++ src/main.js
@@ -5,29 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+// 全局指令注册
+import { lazyPlugin } from '@/directives'
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
-//定义全局指令
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        //el：指令绑定的元素 img
-        //binding: binding.value :指令绑定的值
-        // console.log(el, binding)
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                // console.log(isIntersecting);
-                if (isIntersecting) {
-                    //进入视口区域
-                    el.src = binding.value
-                }
-
-            }
-        )
-    }
-})
