516473575caff3cad1622e0393f29305eb4bde85
yongqi <yongqi@9995gmail.com>
2023-11-17 22:51:52 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/es/components/message/style/css'
+import { useUserStore } from '@/stores/user'
+
 // 创建axios实例
 const http = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -8,7 +10,16 @@ const http = axios.create({
 })
 
 // axios请求拦截器
+
+// 基础思想：很多接口如果想要获取数据必须要带着有效的Token信息才可以，拦截器中做一次，用到axios实例的其他都可以拿到
 http.interceptors.request.use(config => {
+  // 1. 从pinia获取token数据
+  const userStore = useUserStore()
+  // 2. 按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
@@ -18,6 +29,8 @@ http.interceptors.response.use(res => res.data, e => {
   ElMessage({ type: 'warning', message: e.response.data.message})
   return Promise.reject(e)
 })
+
+
 
 
 export default http
